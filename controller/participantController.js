ddb07df54d0e43492f496839b3c95d7c9e1c3f2c
renakaagusta <renakaagusta28@gmail.com>
2021-12-10 05:32:04 +0700

    validate objectid

--- controller/participantController.js
+++ controller/participantController.js
@@ -45,7 +45,7 @@ exports.index = function (req, res) {
 
         participants = [].concat(participants).reverse();
 
-        participants.forEach(function(participant){ delete participant.code });
+        participants.forEach(function (participant) { delete participant.code });
 
         return res.json({
             status: "success",
@@ -59,85 +59,89 @@ exports.index = function (req, res) {
 exports.search = function (req, res) {
 
 
-    const clientIP = 
-    req.headers['x-forwarded-for'] ||
-    req.socket.remoteAddress ||
-    null;
+    const clientIP =
+        req.headers['x-forwarded-for'] ||
+        req.socket.remoteAddress ||
+        null;
     console.log("..clientIp")
     console.log(clientIP)
-    Ip.find({ip: clientIP}, function(err, client){
+    Ip.find({ ip: clientIP }, function (err, client) {
         console.log("..err")
         console.log(err)
         console.log("..client")
         console.log(client)
-        if(client.length > 0) {
-    Participant.find({
-        name: {
-            $regex: req.params.name,
-        },
-    },
-        function (err, participants) {
-            if (err) {
-                return res.json({
-                    status: "error",
-                    message: err,
-                });
-            }
+        if (client.length > 0) {
+            Participant.find({
+                name: {
+                    $regex: req.params.name,
+                },
+            },
+                function (err, participants) {
+                    if (err) {
+                        return res.json({
+                            status: "error",
+                            message: err,
+                        });
+                    }
 
-            participants = [].concat(participants).reverse();
+                    participants = [].concat(participants).reverse();
 
-            return res.json({
-                status: "success",
-                message: "Participant Added Successfully",
-                data: participants,
-            });
+                    return res.json({
+                        status: "success",
+                        message: "Participant Added Successfully",
+                        data: participants,
+                    });
+                }
+            );
         }
-    );}})
+    })
 };
 
 // Handle index actions
 exports.indexByPage = async function (req, res) {
-    
-    const clientIP = 
-    req.headers['x-forwarded-for'] ||
-    req.socket.remoteAddress ||
-    null;
+
+    const clientIP =
+        req.headers['x-forwarded-for'] ||
+        req.socket.remoteAddress ||
+        null;
     console.log("..clientIp")
     console.log(clientIP)
-    Ip.find({ip: clientIP}, async function(err, client){
+    Ip.find({ ip: clientIP }, async function (err, client) {
         console.log("..err")
         console.log(err)
         console.log("..client")
         console.log(client)
-        if(client.length > 0) {
-    var page = req.params.page;
-    try {
-        var totalParticipant = await Participant.count();
-        var participants = await Participant.find()
-            .sort({ _id: -1 })
-            .limit(10)
-            .skip((page - 1) * 10)
-            .exec();
+        if (client.length > 0) {
+            var page = req.params.page;
+            try {
+                var totalParticipant = await Participant.count();
+                var participants = await Participant.find()
+                    .sort({ _id: -1 })
+                    .limit(10)
+                    .skip((page - 1) * 10)
+                    .exec();
 
-        return res.json({
-            status: "success",
-            message: "Participant Added Successfully",
-            data: {
-                participants: participants,
-                totalPage: Math.ceil(totalParticipant / 10),
-            },
-        });
-    } catch (err) {
-        return res.send(err);
-    }}})
+                return res.json({
+                    status: "success",
+                    message: "Participant Added Successfully",
+                    data: {
+                        participants: participants,
+                        totalPage: Math.ceil(totalParticipant / 10),
+                    },
+                });
+            } catch (err) {
+                return res.send(err);
+            }
+        }
+    })
 };
 
 function escapeRegExp(string) {
     return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
-  }
-  function replaceAll(str, match, replacement){
-     return str.replace(new RegExp(escapeRegExp(match), 'g'), ()=>replacement);
-  }
+}
+function replaceAll(str, match, replacement) {
+    return str.replace(new RegExp(escapeRegExp(match), 'g'), () => replacement);
+}
 
 // Handle view actions
 exports.view = function (req, res) {
@@ -145,39 +149,45 @@ exports.view = function (req, res) {
     let checkForValidMongoDbID = new RegExp("^[0-9a-fA-F]{24}$");
 
     if (req.params.id.length < 25) {
-        
-    const clientIP = 
-    req.headers['x-forwarded-for'] ||
-    req.socket.remoteAddress ||
-    null;
-    console.log("..clientIp")
-    console.log(clientIP)
-    Ip.find({ip: clientIP}, function(err, client){
-        
-        if(client.length > 0) {
-        const id = mongoose.Types.ObjectId(req.params.id)
-        Participant.findById(id, function (err, participant) {
-            if (id.match(/^[0-9a-fA-F]{24}$/)) {
-                
-        } else {return res.json({
-            message: "participants Detail Loading...",
-            data: {},
-        });   
-        }
 
-            
-            console.log(".helo")
-            console.log(err)
-            return res.json({
-                message: "participants Detail Loading...",
-                data: participant,
-            });
-        });}})
+        const clientIP =
+            req.headers['x-forwarded-for'] ||
+            req.socket.remoteAddress ||
+            null;
+        console.log("..clientIp")
+        console.log(clientIP)
+        Ip.find({ ip: clientIP }, function (err, client) {
+
+            if (client.length > 0) {
+                if (id.match(/^[0-9a-fA-F]{24}$/)) {
+
+                } else {
+                    console.log("..invalid id")
+                    console.log(id)
+                    return res.json({
+                        message: "participants Detail Loading...",
+                        data: {},
+                    });
+                }
+                const id = mongoose.Types.ObjectId(req.params.id)
+                Participant.findById(id, function (err, participant) {
+
+
+
+                    console.log(".helo")
+                    console.log(err)
+                    return res.json({
+                        message: "participants Detail Loading...",
+                        data: participant,
+                    });
+                });
+            }
+        })
     } else {
-        const chipertext = replaceAll(req.params.id.toString(),"8---8", '/')
-        
+        const chipertext = replaceAll(req.params.id.toString(), "8---8", '/')
+
         const email = CryptoJS.AES.decrypt(chipertext, "voting-sv-okeoke").toString(CryptoJS.enc.Utf8);
-        
+
         console.log("..email")
         console.log(email)
         Participant.findOne({
@@ -195,146 +205,150 @@ exports.view = function (req, res) {
 
 // Handle create actions
 exports.new = function (req, res) {
-    
-    const clientIP = 
-    req.headers['x-forwarded-for'] ||
-    req.socket.remoteAddress ||
-    null;
+
+    const clientIP =
+        req.headers['x-forwarded-for'] ||
+        req.socket.remoteAddress ||
+        null;
     console.log("..clientIp")
     console.log(clientIP)
-    Ip.find({ip: clientIP}, function(err, client){
+    Ip.find({ ip: clientIP }, function (err, client) {
         console.log("..err")
         console.log(err)
         console.log("..client")
         console.log(client)
-        if(client.length > 0) {
-    var participant = new Participant();
-    participant.name = req.body.name;
-    participant.nim = req.body.nim;
-    participant.email = req.body.email;
-    participant.code = CryptoJS.AES.encrypt(req.body.email, "voting-sv-okeoke").toString();
-    participant.code = replaceAll(participant.code, '/', '8---8');
-    participant.subject = req.body.subject ? req.body.subject : '1';
-    participant.session.id = req.body.sessionId;
-    participant.session.number = req.body.sessionNumber;
-    participant.session.min = new Date(req.body.sessionMin);
-    participant.session.max = new Date(req.body.sessionMax);
+        if (client.length > 0) {
+            var participant = new Participant();
+            participant.name = req.body.name;
+            participant.nim = req.body.nim;
+            participant.email = req.body.email;
+            participant.code = CryptoJS.AES.encrypt(req.body.email, "voting-sv-okeoke").toString();
+            participant.code = replaceAll(participant.code, '/', '8---8');
+            participant.subject = req.body.subject ? req.body.subject : '1';
+            participant.session.id = req.body.sessionId;
+            participant.session.number = req.body.sessionNumber;
+            participant.session.min = new Date(req.body.sessionMin);
+            participant.session.max = new Date(req.body.sessionMax);
 
-    console.log("..body")
-    console.log(req.body)
-    
-
-    // Save and validate
-    participant.save(function (err) {
-        console.log("..err");
-        console.log(err)
-        if (err) return res.status(500).json(err);
+            console.log("..body")
+            console.log(req.body)
 
 
-        Session.findById(req.body.sessionId, function (err, session) {
+            // Save and validate
+            participant.save(function (err) {
+                console.log("..err");
+                console.log(err)
+                if (err) return res.status(500).json(err);
 
-            console.log("..err2");
-            console.log(err)
-            if (err) return res.status(500).json(err);
 
-            console.log(err)
-            session.total_participant++;
-            Session.findOneAndUpdate({ _id: session._id }, { $set: session }).then(
-                (session) => {
-                    if (session) { } else { }
-                }
-            );
-        });
+                Session.findById(req.body.sessionId, function (err, session) {
 
-        return res.json({
-            message: "New Participant Created!",
-            data: participant,
-        });
-    });}})
+                    console.log("..err2");
+                    console.log(err)
+                    if (err) return res.status(500).json(err);
+
+                    console.log(err)
+                    session.total_participant++;
+                    Session.findOneAndUpdate({ _id: session._id }, { $set: session }).then(
+                        (session) => {
+                            if (session) { } else { }
+                        }
+                    );
+                });
+
+                return res.json({
+                    message: "New Participant Created!",
+                    data: participant,
+                });
+            });
+        }
+    })
 };
 
 // Handle update actions
 exports.update = function (req, res) {
 
-    const clientIP = 
-    req.headers['x-forwarded-for'] ||
-    req.socket.remoteAddress ||
-    null;
+    const clientIP =
+        req.headers['x-forwarded-for'] ||
+        req.socket.remoteAddress ||
+        null;
     console.log("..clientIp")
     console.log(clientIP)
-    Ip.find({ip: clientIP}, function(err, client){
+    Ip.find({ ip: clientIP }, function (err, client) {
         console.log("..err")
         console.log(err)
         console.log("..client")
         console.log(client)
-        if(client.length > 0) {
-    var moveSession = false;
-    var oldSession = {};
-    var newSession = {};
+        if (client.length > 0) {
+            var moveSession = false;
+            var oldSession = {};
+            var newSession = {};
 
-    Participant.findById(req.params.id, function (err, participant) {
-        if (err) throw err;
-        if (participant.session.id != req.body.sessionId) {
-            moveSession = true;
-            oldSession = participant.session;
-            newSession = {
-                id: req.body.sessionId,
-                number: req.body.sessionNumber,
-                start: new Date(req.body.sessionMin),
-                end: new Date(req.body.sessionMax),
-            };
-        }
-    });
-
-    Participant.findOneAndUpdate({ _id: req.params.id }, {
-        $set: {
-            name: req.body.name,
-            nim: req.body.nim,
-            email: req.body.email,
-            subject: req.body.subject,
-            "session.id": req.body.sessionId,
-            "session.number": req.body.sessionNumber,
-            "session.min": new Date(req.body.sessionMin),
-            "session.max": new Date(req.body.sessionMax),
-        },
-    })
-        .then((participant) => {
-            if (participant) {
-                if (moveSession) {
-                    Session.findById(newSession.id, function (err, session) {
-                        if (err) throw err;
-                        session.total_participant++;
-                        Session.findOneAndUpdate({ _id: session._id }, { $set: session }).then((session) => {
-                            if (session) { } else { }
-                        });
-                    });
-
-                    Session.findById(oldSession.id, function (err, session) {
-                        if (err) throw err;
-                        session.total_participant--;
-                        Session.findOneAndUpdate({ _id: session._id }, { $set: session }).then((session) => {
-                            if (session) { } else { }
-                        });
-                    });
+            Participant.findById(req.params.id, function (err, participant) {
+                if (err) throw err;
+                if (participant.session.id != req.body.sessionId) {
+                    moveSession = true;
+                    oldSession = participant.session;
+                    newSession = {
+                        id: req.body.sessionId,
+                        number: req.body.sessionNumber,
+                        start: new Date(req.body.sessionMin),
+                        end: new Date(req.body.sessionMax),
+                    };
                 }
-
-                return res.json({
-                    message: "participant updated",
-                    data: participant,
-                });
-            } else {
-                return res.json({
-                    message: "participant not found",
-                    data: {},
-                });
-            }
-        })
-        .catch((err) => {
-            res.json({
-                message: "error",
-                data: err,
             });
-        });}})
+
+            Participant.findOneAndUpdate({ _id: req.params.id }, {
+                $set: {
+                    name: req.body.name,
+                    nim: req.body.nim,
+                    email: req.body.email,
+                    subject: req.body.subject,
+                    "session.id": req.body.sessionId,
+                    "session.number": req.body.sessionNumber,
+                    "session.min": new Date(req.body.sessionMin),
+                    "session.max": new Date(req.body.sessionMax),
+                },
+            })
+                .then((participant) => {
+                    if (participant) {
+                        if (moveSession) {
+                            Session.findById(newSession.id, function (err, session) {
+                                if (err) throw err;
+                                session.total_participant++;
+                                Session.findOneAndUpdate({ _id: session._id }, { $set: session }).then((session) => {
+                                    if (session) { } else { }
+                                });
+                            });
+
+                            Session.findById(oldSession.id, function (err, session) {
+                                if (err) throw err;
+                                session.total_participant--;
+                                Session.findOneAndUpdate({ _id: session._id }, { $set: session }).then((session) => {
+                                    if (session) { } else { }
+                                });
+                            });
+                        }
+
+                        return res.json({
+                            message: "participant updated",
+                            data: participant,
+                        });
+                    } else {
+                        return res.json({
+                            message: "participant not found",
+                            data: {},
+                        });
+                    }
+                })
+                .catch((err) => {
+                    res.json({
+                        message: "error",
+                        data: err,
+                    });
+                });
+        }
+    })
 };
 
 // Handle vote actions
